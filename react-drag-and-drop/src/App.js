4b67e95f0ab9927a572480a74265a118f2ebaee4
S.M. Shabab <42778250+smshabab@users.noreply.github.com>
2022-03-18 13:31:49 +0600

    init

---
+++ react-drag-and-drop/src/App.js
@@ -0,0 +1,25 @@
+import Board from "./components/Board";
+import Card from "./components/Card";
+import './App.css';
+
+function App() {
+  return (
+    <div className="flexbox">
+
+      <Board id="boadr-1" className="board">
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card One</p>
+        </Card>
+      </Board>
+
+      <Board id="boadr-2" className="board">
+        <Card id="card-2" className="card" draggable="true">
+          <p>Card Two</p>
+        </Card>
+      </Board>
+      
+    </div>
+  );
+}
+
+export default App;
