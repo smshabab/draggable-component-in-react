5784c6dbac2526580c2520de060ed7707608e833
S.M. Shabab <42778250+smshabab@users.noreply.github.com>
2022-03-18 19:51:56 +0600

    Update

--- react-drag-and-drop/src/App.js
+++ react-drag-and-drop/src/App.js
@@ -7,12 +7,13 @@ function App() {
     <div className="flexbox">
 
       <Board id="boadr-1" className="board">
-        <Card id="card-1" className="card" draggable="true">
-          <p>Card One</p>
-        </Card>
+       
       </Board>
 
       <Board id="boadr-2" className="board">
+        <Card id="card-1" className="card" draggable="true">
+          <p>Card One</p>
+        </Card>
         <Card id="card-2" className="card" draggable="true">
           <p>Card Two</p>
         </Card>
